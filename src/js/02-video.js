bfec2c0d4a22c9feec9c2feef634b73f4606d5ff
HelgaNaz <onworkit@gmail.com>
2021-11-22 14:51:37 +0200

    task 1-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle (function () {
+    
+    player.getCurrentTime().then(function(seconds) {
+        localStorage.setItem("videoplayer-current-time", seconds);
+}).catch(function(error) {
+    console.log(error);
+});
+
+}, 1000));
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function (seconds) {
+   console.log("Begin", seconds)
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            console.log("The time was less than 0 or greater than the video’s duration")
+            break;
+        default:
+            console.log("Some other error occurred")
+            break;
+    }
+});
+
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector(".feedback-form");
+const formInput = document.querySelector("input");
+const formTextarea = document.querySelector("textarea");
+const formMessage = {};
+
+form.addEventListener("input", throttle(saveFunction, 500));
+
+function saveFunction(event) {
+    event.preventDefault();
+    const {elements: { email, message }} = event.currentTarget;
+
+    if (email.value === "" || message.value === "") {
+        return console.log("Please fill in all the fields!");
+    };
+
+    formMessage.email = email.value;
+    formMessage.message = message.value;
+    
+    localStorage.setItem("feedback-form-state", JSON.stringify(formMessage));
+    console.log("SaveMessage", formMessage);
+}
+const localSave = JSON.parse(localStorage.getItem("feedback-form-state"));
+
+if(localSave) {
+    formInput.value = localSave.email;
+    formTextarea.value = localSave.message;
+}
+
+form.addEventListener("submit", submitFunction);
+
+function submitFunction(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+    localStorage.removeItem("feedback-form-state");
+    console.log("SubmitMessage", formMessage);
+}
