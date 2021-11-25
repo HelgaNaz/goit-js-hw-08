94a403f019e0e71fbe4ae8507001ebf55523d844
HelgaNaz <onworkit@gmail.com>
2021-11-25 14:16:57 +0200

    task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,38 +1,45 @@
 import throttle from 'lodash.throttle';
+import { debounce } from 'debounce';
 
 const form = document.querySelector(".feedback-form");
 const formInput = document.querySelector("input");
 const formTextarea = document.querySelector("textarea");
-const formMessage = {};
+const formMessage = {email: "", message: ""};
 
 form.addEventListener("input", throttle(saveFunction, 500));
 
 function saveFunction(event) {
-    event.preventDefault();
-    const {elements: { email, message }} = event.currentTarget;
-
-    if (email.value === "" || message.value === "") {
-        return console.log("Please fill in all the fields!");
-    };
-
-    formMessage.email = email.value;
-    formMessage.message = message.value;
     
+    if (event.target === formInput) {
+        formMessage.email = event.target.value;
+    }
+    else if (event.target === formTextarea) {
+        formMessage.message = event.target.value;
+    }
+
     localStorage.setItem("feedback-form-state", JSON.stringify(formMessage));
     console.log("SaveMessage", formMessage);
+
 }
 const localSave = JSON.parse(localStorage.getItem("feedback-form-state"));
 
-if(localSave) {
-    formInput.value = localSave.email;
-    formTextarea.value = localSave.message;
+document.addEventListener("DOMContentLoaded", update())
+
+function update() {
+    formInput.value = localSave?.email || "";
+    formTextarea.value = localSave?.message || "";
+    console.log(localSave);
+
 }
 
 form.addEventListener("submit", submitFunction);
 
 function submitFunction(event) {
     event.preventDefault();
-    event.currentTarget.reset();
-    localStorage.removeItem("feedback-form-state");
+   
     console.log("SubmitMessage", formMessage);
+    event.target.reset();
+    localStorage.removeItem("feedback-form-state");
 }
+
+
